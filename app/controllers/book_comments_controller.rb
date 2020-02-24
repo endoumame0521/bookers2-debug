5297a41fba6657f85e566f23d2c57131117197b9
endoumame0521 <1125katanohimeji0521@gmail.com>
2020-02-24 10:00:41 +0000

    Added the function of ajax favorite and comment 2020.02.24 18:59

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,23 +1,23 @@
 class BookCommentsController < ApplicationController
+	before_action :set_book, only: [:create, :destroy]
+
 	def create
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.user_id = current_user.id
 		@book_comment.book_id = params[:book_id]
-		if @book_comment.save
-			redirect_to book_path(params[:book_id]), notice: "successfully submitted comment!"
-		else
-    		@book = Book.find(params[:book_id])
-			render "books/show"
-		end
+		@book_comment.save
 	end
 
 	def destroy
 		book_comment = current_user.book_comments.find_by(book_id: params[:book_id], id: params[:id])
 		book_comment.destroy
-		redirect_to book_path(params[:book_id])
 	end
 
 	private
+	def set_book
+    	@book = Book.find(params[:book_id])
+	end
+
 	def book_comment_params
 		params.require(:book_comment).permit(:comment)
 	end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,20 @@
 class FavoritesController < ApplicationController
+	before_action :set_book
+
 	def create
 		favorite = Favorite.new
 		favorite.user_id = current_user.id
 		favorite.book_id = params[:book_id]
 		favorite.save
-		redirect_to request.referrer
 	end
 
 	def destroy
 		favorite = current_user.favorites.find_by(book_id: params[:book_id])
 		favorite.destroy
-		redirect_to request.referrer
 	end
 
+	private
+	def set_book
+		@book = Book.find(params[:book_id])
+	end
 end
